381ab463b6faf97b5ae1218bb97c626ee607facd
EduardMarrugo <camilo.ecmm@gmail.com>
2023-05-14 18:33:50 -0500

    DbzModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 
 import { CounterModule } from './counter/counter.module';
 import { HeroeModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CounterModule, HeroeModule],
+  imports: [BrowserModule, CounterModule, HeroeModule, DbzModule],
   providers: [],
   bootstrap: [AppComponent],
 })
