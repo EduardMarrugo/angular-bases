de35bdbfefd73c253ab15aa4baad5e1a0284f127
EduardMarrugo <camilo.ecmm@gmail.com>
2023-05-18 12:15:48 -0500

    Commit

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: 'main-page.component.html',
 })
 export class MainPageComponent {
-  constructor() {}
+  public characters: Character[] = [
+    {
+      name: 'Goku',
+      power: 1000,
+    },
+    {
+      name: 'Krillin',
+      power: 350,
+    },
+  ];
+  public onNewCharacter(character: Character): void {
+    console.log(character);
+    this.characters.push(character);
+  }
 }
